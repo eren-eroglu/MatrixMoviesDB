fa462a29c5cc92217277bc0a1e479074f722756f
Eren <vrsozluk@gmail.com>
2023-03-26 13:39:33 +0300

    search component and movies component has been added and done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { AppProvider } from "./context";
+import { BrowserRouter as Router } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppProvider>
+      <Router>
+        <App />
+      </Router>
+    </AppProvider>
+  </React.StrictMode>
+);
